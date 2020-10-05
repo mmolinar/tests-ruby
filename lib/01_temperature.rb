a8ae47154f6d217dc7c253f83f5668d17fc4985b
mmolinar <mariamolinaryarza@gmail.com>
2020-10-05 14:32:23 +0200

    Init commit

---
+++ lib/01_temperature.rb
@@ -0,0 +1,7 @@
+def ftoc(number)
+  return (number - 32)* 5/9
+end
+
+def ctof(number)
+  return (number * 9.0/5) + 32
+end
---
+++ lib/02_calculator.rb
@@ -0,0 +1,23 @@
+def add(arg1, arg2)
+  return arg1 + arg2
+end
+
+def subtract(arg1, arg2)
+  return arg1 - arg2
+end
+
+def sum(arr)
+  return (arr.sum)
+end
+
+def multiply(num1, num2)
+  return num1 * num2
+end
+
+def power(n1, n2)
+  return n1**n2
+end
+
+def factorial(n)
+  return n < 1 ? 1 : factorial(n-1)*n
+end
